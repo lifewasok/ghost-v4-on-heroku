34a8909fdf9fd541ca286d79077b8f819dcaf3f7
lifewasok <96996851+lifewasok@users.noreply.github.com>
2022-01-02 19:25:45 -0700

    feat: added postgres env vars

--- bin/common/env-values.js
+++ bin/common/env-values.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mysqlDatabaseUrl: process.env.MYSQL_DATABASE_URL || process.env.JAWSDB_URL || process.env.CLEARDB_DATABASE_URL
+  mysqlDatabaseUrl: process.env.MYSQL_DATABASE_URL || process.env.JAWSDB_URL || process.env.CLEARDB_DATABASE_URL,
+  postgresqlDatabaseUrl: process.env.DATABASE_URL || process.env.POSTGRES_URL || process.env.PGDB_URL
 }
